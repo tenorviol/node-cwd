cb5ad45ba98d36efe732a290273a711846e23796
Chris Johnson <tenorviol@yahoo.com>
2012-12-23 11:31:40 -0800

    Made loading of local lib directory contingent on there actually being a local lib directory

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var fs = require('fs');
+
+// to the lib loader module
 var libLoader = require('./lib/libLoader');
-module.exports = libLoader;
+
+// load the current process's ./lib directory onto the libLoader function
+var libDir = process.cwd() + '/lib';
+var stat = fs.statSync(libDir);
+if (stat.isDirectory()) {
+  var lib = libLoader(process.cwd() + '/lib', libLoader);
+} else {
+  var lib = libLoader;
+}
+
+module.exports = lib;
--- test/libTest.js
+++ test/libTest.js
@@ -1,17 +1,25 @@
-var assert = require('assert');
-var lib = require('../index')(__dirname + '/libTestLib');
+var assert  = require('assert');
+var lib     = require('../index');
+var testLib = lib(__dirname + '/libTestLib');
 
 describe('lib', function () {
-  it('should create getters for all modules', function () {
-    assert.equal(require('./libTestLib/Foo'), lib.Foo);
+
+  it('should create getters for all modules in the directory', function () {
+    assert.equal(require('./libTestLib/Foo'), testLib.Foo);
     it('should work multiple times', function () {
-      assert.equal(require('./libTestLib/Foo'), lib.Foo);
+      assert.equal(require('./libTestLib/Foo'), testLib.Foo);
     })
   });
+
   it('should traverse subdirectories recursively', function () {
-    assert.equal(require('./libTestLib/subdir/Bar'), lib.subdir.Bar);
+    assert.equal(require('./libTestLib/subdir/Bar'), testLib.subdir.Bar);
     it('should work multiple times', function () {
-      assert.equal(require('./libTestLib/subdir/Bar'), lib.subdir.Bar);
+      assert.equal(require('./libTestLib/subdir/Bar'), testLib.subdir.Bar);
     })
   });
+
+  it('should load the current working directory ./lib directory by default', function () {
+    assert.equal(require('../lib/libLoader'), lib.libLoader);
+  });
+
 });
